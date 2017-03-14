b0073d6a660c2386afebd82750c64821868775d9
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-03-14 18:19:27 +0300

    Update worker

--- worker.sh
+++ worker.sh
@@ -2,4 +2,6 @@
 source /opt/FairShipRun/config.sh
 JOBID=$1
 GEOFILE=$2
-/input/slave.py --geofile /input/$GEOFILE --jobid $JOBID -f /input/muons.root --lofi --results /output/result_$JOBID.root
+TOTAL=4
+echo "python2 /input/slave.py --geofile /input/$GEOFILE --jobid $JOBID -f /input/muons_${JOBID}_${TOTAL}.root --lofi --results /output/result_${JOBID}.root"
+python2 /input/slave.py --geofile /input/$GEOFILE --jobid $JOBID -f /input/muons_${JOBID}_${TOTAL}.root --lofi --results /output/result_${JOBID}.root
