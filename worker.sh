11ff21c2e585887185e660b1328321cde7a65b82
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-04-20 18:17:53 +0200

    Use 1600 jobs on skygrid

--- worker.sh
+++ worker.sh
@@ -3,5 +3,5 @@ set -eux
 source /opt/FairShipRun/config.sh
 JOBID=$1
 GEOFILE=$2
-TOTAL=4
+TOTAL=1600
 python2 /input/slave.py --geofile /input/"$GEOFILE" --jobid "$JOBID" -f /input/muons_"${JOBID}"_"${TOTAL}".root --results /output/result.csv
