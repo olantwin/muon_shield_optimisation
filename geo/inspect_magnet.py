faefe43d2288911752c35f77dbd0d131b6156660
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-04-25 17:13:23 +0200

    Remove comments and save eps

--- geo/inspect_magnet.py
+++ geo/inspect_magnet.py
@@ -4,7 +4,5 @@ import rootpy.ROOT as r
 f = r.TFile.Open(sys.argv[1])
 fGeo = f.FAIRGeom
 fGeo.GetVolume("MuonShieldArea").Draw("ogl")
-# fGeo.GetVolume("MuonShieldArea").GetNode("MagnAbsorb1_1").VisibleDaughters(False)
-# fGeo.GetVolume("MuonShieldArea").GetNode("MagnAbsorb1_1").SetVisibility(False)
-# fGeo.GetVolume("MuonShieldArea").GetNode("MagnAbsorb2_1").VisibleDaughters(False)
-# fGeo.GetVolume("MuonShieldArea").GetNode("MagnAbsorb2_1").SetVisibility(False)
+r.gPad.SaveAs("shield.eps")
+
