dbe2b82bf761645008cc6e6fc2c172aa14b38c8d
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-04-25 21:00:09 +0200

    Update resampling

--- filter.py
+++ filter.py
@@ -3,7 +3,8 @@ import argparse
 from array import array
 import numpy as np
 import rootpy.ROOT as r
-from rootpy.plotting import Canvas, Hist2D
+from rootpy.plotting import Hist2D
+from rootpy.tree import Ntuple
 
 
 def cap_bins(intuple, binmax=100):
@@ -16,7 +17,7 @@ def cap_bins(intuple, binmax=100):
         pz = muon.pz
         p = np.sqrt(px**2 + py**2 + pz**2)
         pt = np.sqrt(px**2 + py**2)
-        b = int(p / 350. * 100)+1, int(pt / 6. * 100)+1
+        b = int(p / 350. * 100) + 1, int(pt / 6. * 100) + 1
         h_all.Fill(p, pt)
         if h[b].value < binmax:
             h.Fill(p, pt)
@@ -34,24 +35,30 @@ def fill_up_bins(intuple, h, binmin=10):
         pz = muon.pz
         p = np.sqrt(px**2 + py**2 + pz**2)
         pt = np.sqrt(px**2 + py**2)
-        b = int(p / 350. * 100)+1, int(pt / 6. * 100)+1
+        b = int(p / 350. * 100) + 1, int(pt / 6. * 100) + 1
         a = array('f', [y for x in muon.values() for y in x])
         if h[b].value < binmin:
-            n = int(binmin/h[b].value)
+            n = int(binmin / h[b].value)
             for _ in range(n):
                 phi = r.gRandom.Uniform(0., 2.) * r.TMath.Pi()
                 px = pt * r.TMath.Cos(phi)
                 py = pt * r.TMath.Sin(phi)
                 a[1] = px
                 a[2] = py
+                a[7] = px
+                a[8] = py
                 h_added_random.Fill(p, pt)
                 intuple.Fill(a)
-        a[1] = + pt
+        a[1] = +pt
         a[2] = 0
+        a[7] = +pt
+        a[8] = 0
         intuple.Fill(a)
         h_added_mirror.Fill(p, pt)
-        a[1] = - pt
+        a[1] = -pt
         a[2] = 0
+        a[7] = +pt
+        a[8] = 0
         intuple.Fill(a)
         h_added_mirror.Fill(p, pt)
     return intuple, h_added_random, h_added_mirror
@@ -59,7 +66,8 @@ def fill_up_bins(intuple, h, binmin=10):
 
 def add_problematic(intuple, n=2000):
     # TODO why aren't these saved?
-    h_added_problematic = Hist2D(100, 0, 350, 100, 0, 6, name='added_problematic')
+    h_added_problematic = Hist2D(
+        100, 0, 350, 100, 0, 6, name='added_problematic')
     pt = 2.5
     for _ in range(n):
         p = r.gRandom.Uniform(150., 350.)
@@ -67,9 +75,21 @@ def add_problematic(intuple, n=2000):
         phi = r.gRandom.Uniform(0., 2.) * r.TMath.Pi()
         px = pt * r.TMath.Cos(phi)
         py = pt * r.TMath.Sin(phi)
-        intuple.Fill(13., px, py, pz, 0, 0, -49.79, 0, 0, -49.79, 0, 0, 1, 0)
-        intuple.Fill(13., +pt, 0, pz, 0, 0, -49.79, 0, 0, -49.79, 0, 0, 1, 0)
-        intuple.Fill(13., -pt, 0, pz, 0, 0, -49.79, 0, 0, -49.79, 0, 0, 1, 0)
+        intuple.Fill(
+            array('f', [
+                13., px, py, pz, 0, 0, -49.79, px, py, pz, 0, 0, -49.79, 0, 0,
+                1, 0
+            ]))
+        intuple.Fill(
+            array('f', [
+                13., +pt, 0, pz, 0, 0, -49.79, +pt, 0, pz, 0, 0, -49.79, 0, 0,
+                1, 0
+            ]))
+        intuple.Fill(
+            array('f', [
+                13., -pt, 0, pz, 0, 0, -49.79, -pt, 0, pz, 0, 0, -49.79, 0, 0,
+                1, 0
+            ]))
         h_added_problematic.Fill(p, pt)
         h_added_problematic.Fill(p, pt)
         h_added_problematic.Fill(p, pt)
@@ -77,40 +97,53 @@ def add_problematic(intuple, n=2000):
 
 
 def add_seed(intuple):
-    intuple.create_branches({'seed': 'F'})
-    outtuple = intuple.CloneTree(0)
-    # outtuple.create_branches({'seed': 'F'})
-    for muon in intuple:
+    outtuple = Ntuple(
+        ('id', 'px', 'py', 'pz', 'x', 'y', 'z', 'opx', 'opy', 'opz', 'ox',
+         'oy', 'oz', 'pythiaid', 'parentid', 'w', 'ecut', 'seed'),
+        name='pythia8-Geant4')
+    for i, muon in enumerate(intuple):
         a = array('f', [y for x in muon.values() for y in x])
-        muon.seed = hash(sum(a))
-        a = array('f', [y for x in muon.values() for y in x])
-        outtuple.Fill(a)
+        a.append(float(i))
+        outtuple.fill(a)
     return outtuple
 
 
 def main():
     f = r.TFile.Open(args.input, 'read')
     intuple = f.Get('pythia8-Geant4')
+    print intuple.GetEntries()
     out = r.TFile.Open(args.output, 'recreate')
     outtuple, h, h_all = cap_bins(intuple)
     h.Write()
     h_all.Write()
     outtuple.Write()
+    print outtuple.GetEntries()
     del outtuple
     intuple = out.Get('pythia8-Geant4')
     intuple, h_added_random, h_added_mirror = fill_up_bins(intuple, h)
     intuple.Write()
+    print intuple.GetEntries()
     del intuple
     intuple = out.Get('pythia8-Geant4')
     intuple, h_added_problematic = add_problematic(intuple)
     intuple.Write()
+    print intuple.GetEntries()
+    h_final = Hist2D(100, 0, 350, 100, 0, 6, name='final')
+    for muon in intuple:
+        px = muon.px
+        py = muon.py
+        pz = muon.pz
+        p = np.sqrt(px**2 + py**2 + pz**2)
+        pt = np.sqrt(px**2 + py**2)
+        h_final.Fill(p, pt)
     del intuple
     h_added_problematic.Write()
     h_added_random.Write()
     h_added_mirror.Write()
-    # intuple = out.Get('pythia8-Geant4')
-    # intuple = add_seed(intuple)
-    # intuple.Write()
+    h_final.Write()
+    intuple = out.Get('pythia8-Geant4')
+    intuple = add_seed(intuple)
+    intuple.Write()
 
 
 if __name__ == '__main__':
