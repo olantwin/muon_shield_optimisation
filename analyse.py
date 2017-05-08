27d61070f9478c12847a268622e2b83b62218127
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-05-08 17:45:10 +0200

    Add track drawring, more plots etc. to analysis

--- analyse.py
+++ analyse.py
@@ -1,10 +1,55 @@
 #!/usr/bin/env python2
+"""Functions to analyse and visualise simulation results."""
 import argparse
+from array import array
 import numpy as np
 import ROOT as r
+from rootpy.tree import Ntuple
+from rootpy.plotting import Canvas
 import shipunit as u
 import rootUtils as ut
 from common import get_geo, FCN
+
+
+def graph_tracks(event):
+    """Create graphs of tracks in event by interpolating between vetoPoints."""
+    hits = []
+    fPos = r.TVector3()
+    for n, track in enumerate(event.MCTrack):
+        track.GetStartVertex(fPos)
+        hitlist = {}
+        hitlist[fPos.Z()] = [
+            fPos.X(), fPos.Y(), fPos.Px(), fPos.Py(), fPos.Pz()
+        ]
+        # loop over all sensitive volumes to find hits
+        for hit in event.vetoPoint:
+            if hit.GetTrackID() == (n - 1):
+                lp = hit.LastPoint()
+                lm = hit.LastMom()
+                assert not (lp.x() == lp.y() and lp.x() == lp.z() and
+                            lp.x() == 0)
+                # must be old data, don't expect hit at 0,0,0
+                # first point:
+                # Entry point as p in centre of volume, halfway between
+                # entry and exit
+                hitlist[2. * hit.GetZ() - lp.z()] = [
+                    2. * hit.GetX() - lp.x(), 2. * hit.GetY() - lp.y(), 0.0,
+                    0.0, 0.0
+                ]
+                # last point:
+                hitlist[lp.z()] = [lp.x(), lp.y(), lm.Px(), lm.Py(), lm.Pz()]
+        if len(hitlist) == 1:
+            if track.GetMotherId() < 0:
+                continue
+
+        zs = hitlist.keys()
+        for z in zs:
+            hits.append([hitlist[z][0], hitlist[z][1], z])
+    # sort in z
+    hits.sort(key=lambda x: x[2])
+    xs, ys, zs = zip(*hits)
+    return r.TGraph(len(hits), array('f', zs), array('f', xs)), r.TGraph(
+        len(hits), array('f', zs), array('f', ys))
 
 
 def analyse(tree, outputfile):
@@ -24,6 +69,10 @@ def analyse(tree, outputfile):
         Vector of hit x-positions [cm]
 
     """
+    # TODO handle addition of new output files!
+    r.gROOT.SetBatch(True)
+    maxpt = 6.5
+    maxp = 360.
     h = {}
     ut.bookHist(h, 'mu_pos', '#mu- hits;x[cm];y[cm]', 100, -1000, +1000, 100,
                 -800, 1000)
@@ -33,16 +82,22 @@ def analyse(tree, outputfile):
                 -800, 1000)
     ut.bookHist(h, 'anti-mu_w_pos', '#mu+ hits;x[cm];y[cm]', 100, -1000, +1000,
                 100, -800, 1000)
-    ut.bookHist(h, 'mu_p', '#mu+-;p[GeV];', 100, 0, 350)
-    ut.bookHist(h, 'mu_p_original', '#mu+-;p[GeV];', 100, 0, 350)
-    ut.bookHist(h, 'mu_pt_original', '#mu+-;p_t[GeV];', 100, 0, 6)
-    ut.bookHist(h, 'mu_ppt_original', '#mu+-;p[GeV];p_t[GeV];', 100, 0, 350,
-                100, 0, 6)
-    ut.bookHist(h, 'smear', '#mu+- initial vertex;x[cm];y[cm]', 100, -100,
-                +100, 100, -100, 100)
+    ut.bookHist(h, 'mu_p', '#mu+-;p[GeV];', 100, 0, maxp)
+    ut.bookHist(h, 'mu_p_original', '#mu+-;p[GeV];', 100, 0, maxp)
+    ut.bookHist(h, 'mu_pt_original', '#mu+-;p_t[GeV];', 100, 0, maxpt)
+    ut.bookHist(h, 'mu_ppt_original', '#mu+-;p[GeV];p_t[GeV];', 100, 0, maxp,
+                100, 0, maxpt)
+    ut.bookHist(h, 'smear', '#mu+- initial vertex;x[cm];y[cm]', 100, -10, +10,
+                100, -10, 10)
     xs = r.std.vector('double')()
     i, n = 0, tree.GetEntries()
     print '0/{}\r'.format(n),
+    fout = r.TFile.Open('gallery.root', 'recreate')
+    fout2 = r.TFile.Open('misis.root', 'recreate')
+    fout2.cd()
+    outtuple = Ntuple(
+        ('px', 'py', 'pz', 'x', 'y', 'z', 'opx', 'opy', 'opz', 'w'),
+        name='MISIS')
     mom = r.TVector3()
     for event in tree:
         i += 1
@@ -51,6 +106,7 @@ def analyse(tree, outputfile):
         original_muon = event.MCTrack[1]
         h['smear'].Fill(original_muon.GetStartX(), original_muon.GetStartY())
         for hit in event.vetoPoint:
+            draw = False
             if hit:
                 if not hit.GetEnergyLoss() > 0:
                     continue
@@ -79,11 +135,63 @@ def analyse(tree, outputfile):
                             h['mu_w_pos'].Fill(x, y, w)
                         else:
                             h['anti-mu_w_pos'].Fill(-x, y, w)
+                    if (P > 1) and (x < 2.6 * u.m) and (abs(y) < 5 * u.m):
+                        draw = 4 if pid > 0 else 6
+        if draw:
+            graph_x, graph_y = graph_tracks(event)
+            graph_x.SetLineColor(draw)
+            graph_y.SetLineColor(draw)
+            c = Canvas(1600, 900, name='c{}'.format(i))
+            multigraph = r.TMultiGraph(
+                'tracks_{}'.format(i),
+                'Tracks in acceptance and/or on wrong side;z [cm];x/y [cm]')
+            graph_x.SetLineStyle(1)
+            graph_x.SetMarkerStyle(20)
+            graph_x.SetTitle('x-projection')
+            graph_x.SetFillStyle(0)
+            graph_y.SetTitle('y-projection')
+            graph_y.SetLineStyle(2)
+            graph_y.SetMarkerStyle(20)
+            graph_y.SetFillStyle(0)
+            multigraph.Add(graph_x, 'lp')
+            multigraph.Add(graph_y, 'lp')
+            multigraph.Draw('Alp')
+            c.BuildLegend()
+            fout.cd()
+            c.Write()
+            fout2.cd()
+        hitlist = {}
+        for hit in event.vetoPoint:
+            lp = hit.LastPoint()
+            lm = hit.LastMom()
+            if lp.z() < 2597:
+                hitlist[lp.z()] = [lp.x(), lp.y(), lm.Px(), lm.Py(), lm.Pz()]
+        zs = hitlist.keys()
+        if len(zs) > 1:
+            zs.sort()
+        if not hitlist:
+            continue
+        last_hit = hitlist[zs[-1]]
+        if last_hit[4] > 0:
+            a = array('f',
+                      (last_hit[2],
+                       last_hit[3],
+                       last_hit[4],
+                       last_hit[0],
+                       last_hit[1],
+                       zs[-1],
+                       original_muon.GetPx(),
+                       original_muon.GetPy(), original_muon.GetPz()))
+            outtuple.fill(a)
+    print 'Loop done'
     ut.writeHists(h, outputfile)
+    fout2.cd()
+    outtuple.Write()
     return xs
 
 
 def main():
+    """Run standalone analysis and print FCN."""
     f = r.TFile.Open(args.input, 'read')
     tree = f.cbmsim
     xs = analyse(tree, args.output)
