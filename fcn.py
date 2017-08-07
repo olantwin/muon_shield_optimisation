864d56b3bdece74ac46f25393c563995fc491382
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-08-07 16:55:33 +0200

    Update cost function

--- fcn.py
+++ fcn.py
@@ -1,4 +1,6 @@
+import numpy as np
+
 
 def FCN(W, Sxi2, L):
     print W, L, Sxi2
-    return 0.01 * (W / 1000) * (1. + Sxi2) / (1. - L / 10000.)
+    return np.exp(W / 1000) * (1. + Sxi2) / (1. - L / 10000.)
