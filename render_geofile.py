6b2241d9fb3ceccbe2ca0821d704a84ceba12fa2
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-08-30 14:29:05 +0200

    Replace rootpy dependency with pyROOT

--- render_geofile.py
+++ render_geofile.py
@@ -2,24 +2,24 @@
 import os
 from time import sleep
 import click
-from rootpy.io import root_open
-import rootpy.ROOT as r
+import ROOT as r
 
 
 @click.command()
 @click.argument('geofile')
 def render_geofile(geofile):
-    with root_open(geofile, 'read') as f:
-        geo = f.FAIRGeom
-        muonshield = geo.GetVolume('MuonShieldArea')
-        muonshield.Draw('ogl')
-        viewer = r.gPad.GetViewer3D()
-        viewer.GetFrame().Resize(1200, 800)
-        sleep(1)
-        filename = os.path.splitext(geofile)[0] + '.png'
-        print filename
-        viewer.SavePicture(str(filename))
-        print 2 * muonshield.GetShape().GetDZ()
+    f = r.TFile.Open(geofile, 'read')
+    geo = f.FAIRGeom
+    muonshield = geo.GetVolume('MuonShieldArea')
+    muonshield.Draw('ogl')
+    viewer = r.gPad.GetViewer3D()
+    viewer.GetFrame().Resize(1200, 800)
+    sleep(1)
+    filename = os.path.splitext(geofile)[0] + '.png'
+    print filename
+    viewer.SavePicture(str(filename))
+    print 2 * muonshield.GetShape().GetDZ()
+    f.Close()
 
 
 if __name__ == '__main__':
