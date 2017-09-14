1de623936852bd0c8ca0ed8dc887bd36f042df19
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-09-14 12:55:36 +0200

    Make render_geofile.py independently executable again, hange resolution

--- render_geofile.py
+++ render_geofile.py
@@ -1,8 +1,12 @@
 #!/bin/env python2
 import os
 from time import sleep
+import click
 import ROOT as r
 
+
+@click.command()
+@click.argument('geofile')
 def render_geofile(geofile):
     f = r.TFile.Open(geofile, 'read')
     geo = f.FAIRGeom
@@ -13,7 +17,10 @@ def render_geofile(geofile):
     sleep(1)
     filename = os.path.splitext(geofile)[0] + '.png'
     print filename
-    viewer.SavePicture(str(filename))
+    viewer.SavePictureWidth(str(filename), 4800)
     print 2 * muonshield.GetShape().GetDZ()
     f.Close()
 
+
+if __name__ == '__main__':
+    render_geofile()
