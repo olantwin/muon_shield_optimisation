7e434c4ae672bd50d98fb3fdd0e19b7f18aa43cb
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-09-22 16:47:46 +0200

    Change to piecewise loss function with constant for heavy configurations

--- fcn.py
+++ fcn.py
@@ -4,4 +4,5 @@ import numpy as np
 def FCN(W, Sxi2, L):
     print W, L, Sxi2
     W_star = 1915820.
-    return (1 + np.exp(10. * (W - W_star) / W_star)) * (1. + Sxi2)
+    return (1 + np.exp(10. * (W - W_star) / W_star)) * (
+        1. + Sxi2) if W <= 3000000 else 1e8
