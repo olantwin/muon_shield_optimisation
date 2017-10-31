1ed916818bbee8d6a1463a6b9ba731dc2e99ccf1
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-10-31 10:55:44 +0100

    Update image tags

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ FIXED_PARAMS = [
 ]
 FIXED_RANGES = [(0, 2), (8, 20)]
 IMAGE = 'olantwin/ship-shield'
-IMAGE_TAG = '20170531'  # '20170818' for T4
+IMAGE_TAG = '20171031_T1'  # '20171031_T4' for T4
 RESULTS_TEMPLATE = {
     'error': None,
     'weight': None,
