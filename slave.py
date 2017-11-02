0411a946b6f3f3a18073501365bccf8a6ac55251
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-11-02 14:30:05 +0100

    Fix parsing once and for all?

--- slave.py
+++ slave.py
@@ -99,15 +99,15 @@ def main():
     tmpl = copy.deepcopy(config.RESULTS_TEMPLATE)
 
     try:
-        paramFile = '/shared/params_{}.root'.format(
-            create_id(
-                ParseParams(args.params.decode('base64'))
-                ))
+        params = ParseParams(args.params.decode('base64'))
     except Exception as e:
         tmpl['error'] = e.__repr__()
         with open(args.results, 'w') as f:
             json.dump(tmpl, f)
         raise
+    paramFile = '/shared/params_{}.root'.format(
+        create_id(params)
+    )
     heavy = '/shared/heavy'
     lockfile = paramFile + '.lock'
 
@@ -117,7 +117,7 @@ def main():
             with lock:
                 tmp_paramFile = generate_geo(
                     paramFile.replace('.', '.tmp.'),
-                    ParseParams(args.params)
+                    params
                 )
                 subprocess.call(
                     [
