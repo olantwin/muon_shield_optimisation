eb711f67bea09c6079ffacaa973ffabbe5bba381
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-11-29 14:33:55 +0100

    Make query_points executable and add print

--- query_points.py
+++ query_points.py
@@ -1,16 +1,17 @@
+#!/bin/env python3
 import pandas as pd
 import numpy as np
 
-import disneylandClient
 from disneylandClient import (
-    Job,
-    RequestWithId,
-    ListJobsRequest
+    ListJobsRequest,
+    new_client
 )
 from disney_optimize import ProcessPoints
 
-stub = disneylandClient.new_client()
+stub = new_client()
 all_jobs = stub.ListJobs(ListJobsRequest(kind='point', how_many=1000)).jobs
 X, y = ProcessPoints(all_jobs)
-pd.DataFrame(np.concatenate(
-    [np.array(X).T, np.array([y])]).T).to_csv('points.csv')
+df = pd.DataFrame(np.concatenate(
+    [np.array(X).T, np.array([y])]).T)
+df.to_csv('points.csv')
+print(df)
