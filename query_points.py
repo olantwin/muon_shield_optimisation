ce1174cdb16501ed6f229bcde45fd3fb95dd4675
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-12-04 11:48:02 +0100

    Name columns in query_points

--- query_points.py
+++ query_points.py
@@ -3,10 +3,7 @@ import json
 import pandas as pd
 import numpy as np
 
-from disneylandClient import (
-    ListJobsRequest,
-    new_client
-)
+from disneylandClient import ListJobsRequest, new_client
 from disney_optimize import ProcessPoints
 
 stub = new_client()
@@ -14,7 +11,9 @@ all_jobs = stub.ListJobs(ListJobsRequest(kind='point', how_many=1000)).jobs
 X, y = ProcessPoints(all_jobs, tag='all')
 ids = [job.id for job in all_jobs]
 tags = [json.loads(point.metadata)['user']['tag'] for point in all_jobs]
-df = pd.DataFrame(np.concatenate(
-    [np.array(X).T, np.array([y]), np.array([ids]), np.array([tags])]).T)
+df = pd.DataFrame(
+    data=np.concatenate(
+        [np.array(X).T, np.array([y]), np.array([ids]), np.array([tags])]).T,
+    columns=list(range(1, 57)) + ['loss', 'job_id', 'tag'])
 df.to_csv('points.csv')
 print(df)
