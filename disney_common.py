08db7cbf4628944fd1975c32fa472227dc7e983e
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-12-19 16:41:30 +0100

    Remove unused argument

--- disney_common.py
+++ disney_common.py
@@ -5,7 +5,7 @@ from skopt.space.space import Integer, Space
 from config import FIXED_PARAMS, FIXED_RANGES
 
 
-def FCN(W, Sxi2, L):
+def FCN(W, Sxi2, _):
     W_star = 1915820.
     return (1 + np.exp(10. * (W - W_star) / W_star)) * (
         1. + Sxi2) if W <= 3e6 else 1e8
