9e70e6a9dfb04c252e8ba08ca9c82b75786925d6
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-12-23 16:19:40 +0100

    Update image to include latest slave.py

--- config.py
+++ config.py
@@ -71,7 +71,7 @@ DEFAULT_POINT = [
 FIXED_RANGES = [(0, 2), (8, 20)]
 FIXED_PARAMS = StripFreeParams(DEFAULT_POINT)
 IMAGE = 'olantwin/ship-shield'
-IMAGE_TAG = '20171207_T1'  # '20171128_T4' for T4
+IMAGE_TAG = '20171222_T1'
 RESULTS_TEMPLATE = {
     'error': 'Some',
     'weight': None,
